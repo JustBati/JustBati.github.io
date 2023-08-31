9617e01e13ffbfe69b82a2744ed3b13425ac72f4
Bruno Diaz Medina <brunodiazm300@gmail.com>
2023-08-31 10:45:31 -0600

    Landing page CV

--- JS/Script.js
+++ JS/Script.js
@@ -1,12 +1,22 @@
-window.onscroll = function() {myFunction()};
-
-var navbar = document.getElementById("navbar");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
+$(document).ready(function() {
+    const carousel = $('.carousel');
+    const items = $('.item');
+  
+    let currentIndex = 0;
+  
+    // Cambiar de item cuando se hace clic en el carrusel
+    carousel.on('click', function() {
+      currentIndex = (currentIndex + 1) % items.length;
+      const displacement = -currentIndex * items.width();
+      carousel.css('transform', `translateX(${displacement}px)`);
+    });
+  });
+$(document).ready(function() {
+    const toggleButton = $('#toggle-button');
+    const socialIcons = $('.social-icons');
+  
+    toggleButton.on('click', function() {
+      socialIcons.slideToggle(); // Mostrar u ocultar íconos al hacer clic
+    });
+  });
+    
